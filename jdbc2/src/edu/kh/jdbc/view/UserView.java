88f0e2ddcc809e3e4eb13ca49f18926f680d6c87
khcmh2509 <khcmh2509@gmail.com>
2025-10-29 13:43:30 +0900

    jdbc2 프로젝트 세팅

---
+++ jdbc2/src/edu/kh/jdbc/view/UserView.java
@@ -0,0 +1,20 @@
+package edu.kh.jdbc.view;
+
+import java.util.Scanner;
+
+import edu.kh.jdbc.model.service.UserService;
+
+// View : 사용자와 직접 상호작용하는 화면(UI)를 담당,
+// (사용자에게) 입력을 받고 결과를 출력하는 역할
+public class UserView {
+	
+	// 필드
+	private UserService service = new UserService();
+	private Scanner sc = new Scanner(System.in);
+
+	public void test() {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
